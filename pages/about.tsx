3c84a572ab8fcc1633c2109ce3d0a26f8d261d4d
Tomoo Suzuki <solosoprani2002@gmail.com>
2022-09-05 21:31:23 +0900

    1st commit

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+ 
+export default function About() {
+    return (
+        <div>
+            <Head>
+                <title>自己紹介タイトル</title>
+                <meta name="description" content="自己紹介概要" />
+            </Head>
+            <h1>自己紹介タイトル</h1>
+        </div>
+    )
+}
